449ce7ea7f5c174f8e4c2dc535f2919638f912fe
ahoang2502 <athoang2502@gmail.com>
2023-08-17 13:55:18 +1000

    add authentication

---
+++ components/ui/modal.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { Dialog } from "./dialog";
+
+interface ModalProps {
+  title: string;
+  description: string;
+  isOpen: boolean;
+  onClose: () => void;
+  children?: React.ReactNode;
+}
+
+const Modal = ({
+  title,
+  description,
+  isOpen,
+  onClose,
+  children,
+}: ModalProps) => {
+  const onChange = (open: boolean) => {
+    if (!open) onClose();
+  };
+
+  return <Dialog>
+    
+  </Dialog>
+};
+
+export default Modal;
