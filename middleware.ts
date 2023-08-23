7aa53345427ac71f784433094624033a66117d1c
ahoang2502 <athoang2502@gmail.com>
2023-08-23 22:41:47 +1000

    .

--- middleware.ts
+++ middleware.ts
@@ -4,8 +4,8 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/nextjs/middleware for more information about configuring your middleware
 export default authMiddleware({
-  publicRoutes: ["/api/:path*"],
-  ignoredRoutes: ["/api/webhook/clerk"],
+  publicRoutes: ["/api/:path*", "/"],
+  
 });
 
 export const config = {
